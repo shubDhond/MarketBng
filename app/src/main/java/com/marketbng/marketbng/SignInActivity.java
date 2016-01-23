8bbbe8845021a150fe5f63951d39a558e72e60a4
shubDhond <shubham.dhond@mail.utoronto.ca>
2016-01-23 02:59:12 -0500

    Added signin and signup pages

--- app/src/main/java/com/marketbng/marketbng/SignInActivity.java
+++ app/src/main/java/com/marketbng/marketbng/SignInActivity.java
@@ -1,26 +1,23 @@
 package com.marketbng.marketbng;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class SignInActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_sign_in, menu);
         return true;
     }
 
@@ -37,15 +34,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void launchSignUp(View view){
-        Intent intent = new Intent(this, SignInActivity.class);
-        startActivity(intent);
-    }
-
-    public void launchSignIn(View view){
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
     }
 }
