9ad6beb2e1038ac367bb1cd51919065acaac1022
HushamH <husham.hassan@mail.utoronto.ca>
2016-01-23 15:51:08 -0500

    Set up the libmuse jar
    Yep

---
+++ app/src/main/java/com/marketbng/marketbng/DataListener.java
@@ -0,0 +1,47 @@
+package com.marketbng.marketbng;
+
+import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.interaxon.libmuse.Accelerometer;
+import com.interaxon.libmuse.AnnotationData;
+import com.interaxon.libmuse.ConnectionState;
+import com.interaxon.libmuse.Eeg;
+import com.interaxon.libmuse.LibMuseVersion;
+import com.interaxon.libmuse.MessageType;
+import com.interaxon.libmuse.Muse;
+import com.interaxon.libmuse.MuseArtifactPacket;
+import com.interaxon.libmuse.MuseConfiguration;
+import com.interaxon.libmuse.MuseConnectionListener;
+import com.interaxon.libmuse.MuseConnectionPacket;
+import com.interaxon.libmuse.MuseDataListener;
+import com.interaxon.libmuse.MuseDataPacket;
+import com.interaxon.libmuse.MuseDataPacketType;
+import com.interaxon.libmuse.MuseFileFactory;
+import com.interaxon.libmuse.MuseFileReader;
+import com.interaxon.libmuse.MuseFileWriter;
+import com.interaxon.libmuse.MuseManager;
+import com.interaxon.libmuse.MusePreset;
+import com.interaxon.libmuse.MuseVersion;
+
+/**
+ * Created by Husham on 16-01-23.
+ */
+public class DataListener {
+}
